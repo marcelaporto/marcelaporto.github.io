e6b0b84a70ac4a73b5f6459f19785fe9ab1aa714
Marcela Porto <marcela.fporto@gmail.com>
2017-04-25 21:44:23 -0700

    Reverse changes for MVP

--- js/init.js
+++ js/init.js
@@ -2,25 +2,9 @@ $(document).ready(function(){
   $('.button-collapse').sideNav();
   $('.parallax').parallax();
   $('.carousel.carousel-slider').carousel({fullWidth: true});
-  const panels = document.querySelectorAll('.panel');
-  console.log(panels)
-  // shift();
-  panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-  panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+  shift();
 });
 
-// var shift = setInterval(function(){
-//     $('.carousel').carousel('next');
-// }, 8000);
-
-function toggleOpen() {
-  console.log('Hello');
-  this.classList.toggle('open');
-}
-
-function toggleActive(e) {
-  console.log(e.propertyName);
-  if (e.propertyName.includes('flex')) {
-    this.classList.toggle('open-active');
-  }
-}
+var shift = setInterval(function(){
+    $('.carousel').carousel('next');
+}, 8000);
