92e87363107b35d676706f1cb7807269d94937ab
Marcela Porto <marcela.fporto@gmail.com>
2017-04-25 21:35:30 -0700

    Adds images to images folder

--- js/init.js
+++ js/init.js
@@ -2,9 +2,25 @@ $(document).ready(function(){
   $('.button-collapse').sideNav();
   $('.parallax').parallax();
   $('.carousel.carousel-slider').carousel({fullWidth: true});
-  shift()
-  });
+  const panels = document.querySelectorAll('.panel');
+  console.log(panels)
+  // shift();
+  panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+  panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+});
 
-var shift = setInterval(function(){
-    $('.carousel').carousel('next');
-}, 8000);
+// var shift = setInterval(function(){
+//     $('.carousel').carousel('next');
+// }, 8000);
+
+function toggleOpen() {
+  console.log('Hello');
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
